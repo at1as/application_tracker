e1672f6b4ecfa06f5bca6cabebc6e24fe812c4fc
Jason Willems <jason@willems.ca>
2015-09-28 22:43:37 -0400

    Really fix migration this time, restrict route for user index

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   root 'sessions#new'
 
   # Nested Controllers
-  resources :users do
+  resources :users, except: [:index] do
     resources :companies do
       resources :contacts
       resources :positions
