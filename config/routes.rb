559bc5ed89344d9180599d8f617a4d33fbf5e3b4
Jason Willems <jason@willems.ca>
2015-10-14 18:22:11 -0400

    Route for filtering company index

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   # Main Page
   root 'sessions#new'
+  
+  # High precedence custom routes
+  get 'users/:user_id/companies/filter', to: 'companies#filter', as: 'filter_companies'
 
   # Nested Controllers
   resources :users, except: [:index] do
@@ -20,5 +23,5 @@ Rails.application.routes.draw do
   get    'signup'  => 'users#new'  
 
   get 'users/:user_id/companies/:id/remove_attachment', to: 'companies#remove_attachment', as: 'remove_company_attachment'
-  
+
 end
